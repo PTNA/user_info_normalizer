13db0b18be2df0c9010a179174335db0dd25d96e
hirata <hirata@piano.or.jp>
2018-08-08 16:37:36 +0900

    各メソッド作成

--- lib/user_info_normalizer.rb
+++ lib/user_info_normalizer.rb
@@ -1,5 +1,65 @@
 require "user_info_normalizer/version"
+require "user_info_normalizer/configuration"
+require 'nkf'
 
 module UserInfoNormalizer
-  # Your code goes here...
+  # ハイフンは'－'.ord.to_s(16) => ff0d に統一
+  HYPHEN_REGEXP = "\u002D|\u30FC|\u2010|\u2011|\u2013|\u2014|\u2015|\u2212|\uFF70|\uFF0d"
+
+  class << self
+    def configure
+      yield configuration
+    end
+
+    def configuration
+      @configuration ||= UserInfoNormalizer::Configuration.new
+    end
+  end
+
+end
+
+class String
+  def normalize_name_kana
+    case UserInfoNormalizer::configuration.name_kana_form
+    when 'ピティナ　タロウ'
+      self.tr(' ', '　').to_katakana.strip
+    else
+      # default: 'ﾋﾟﾃｨﾅ ﾀﾛｳ'
+      self.tr('　', ' ').to_hw_katakana.strip
+    end
+  end
+
+  def normalize_address
+    # 数字の前後で長音などはありえないのでハイフンに直す, 数字も全角に
+    self.tr('0-9', '０-９').gsub(/([０-９])(#{UserInfoNormalizer::HYPHEN_REGEXP})/, '\1－').strip
+  end
+
+  def normalize_zip_code
+    case UserInfoNormalizer::configuration.zip_code_form
+    when '123-4567'
+      self.tr('０-９', '0-9').gsub(/([0-9])(#{UserInfoNormalizer::HYPHEN_REGEXP})/, '\1-').strip
+    else
+      # default: '１２３－４５６７'
+      self.tr('0-9', '０-９').gsub(/([０-９])(#{UserInfoNormalizer::HYPHEN_REGEXP})/, '\1－').strip
+    end
+  end
+
+  def to_hiragana
+    NKF.nkf('-w --hiragana', self)
+  end
+
+  def to_katakana
+    NKF.nkf('-w --katakana', self)
+  end
+
+  # ひらがな、カタカナ混じりのものをすべて半角カナに変換
+  def to_hw_katakana
+    NKF.nkf('-w -Z4', to_katakana)
+  end
+end
+
+class NilClass
+  def normalize_name_kana; end
+  def normalize_zip_code; end
+  def normalize_address; end
 end
---
+++ lib/user_info_normalizer/configuration.rb
@@ -0,0 +1,3 @@
+class UserInfoNormalizer::Configuration
+  attr_accessor :name_kana_form, :address_form, :zip_code_form
+end
