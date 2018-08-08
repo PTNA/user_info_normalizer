e834cb1785e60b7e8ee4dd7bd0b554594bb4a784
hirata masato <hirata@piano.or.jp>
2018-08-08 15:05:15 +0900

    first commit

---
+++ lib/user_info_normalizer.rb
@@ -0,0 +1,5 @@
+require "user_info_normalizer/version"
+
+module UserInfoNormalizer
+  # Your code goes here...
+end
---
+++ spec/user_info_normalizer_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe UserInfoNormalizer do
+  it "has a version number" do
+    expect(UserInfoNormalizer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
